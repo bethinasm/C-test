99754dbefea513a6e85a2ed07263b9bc761253c5
Bethina S.M <bethina.s.moen@gmail.com>
2024-11-14 14:04:27 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Hei, hva heter du?");
+var userInput = Console.ReadLine();
+Console.WriteLine($"Velkommen, {userInput}!");
+
+Console.WriteLine("Hvor gammel er du?");
+var input = Console.ReadLine();
+Console.WriteLine($"Du skrev: {input}");
